63f206243c04a949de00e7eab45090d04468bf4f
Paulo Azoia <pazoia.gomes@gmail.com>
2021-07-02 15:33:13 +0100

    When user add a fish to order:
    It updates localStorage with new data,
    for every added fish localStorage will be updated with new order
    when user refreshes page, data from localStorage is reinstated

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -15,10 +15,25 @@ class App extends React.Component {
 
     componentDidMount() {
         const { params } = this.props.match;
+        // first reinstate our localStorage
+        const localStorageRef = localStorage.getItem(params.storeId);
+        if (localStorageRef) {
+            this.setState({
+                order: JSON.parse(localStorageRef),
+            });
+        }
         this.ref = base.syncState(`${params.storeId}/fishes`, {
             context: this,
             state: "fishes",
         });
+    }
+
+    componentDidUpdate() {
+        // eslint-disable-next-line prettier/prettier
+        localStorage.setItem(
+            this.props.match.params.storeId,
+            JSON.stringify(this.state.order),
+        ); 
     }
 
     componentWillUnmount() {
